4076ff5e5fcb67d5142235911e9f77d0c19ee013
Vinícius Alves Pereira <viniciusvap1999@gmail.com>
2018-05-12 15:00:08 -0300

    Adicionado comandos para ler e informar velocidade. Próximo passo é testar e verificar necessidade de interrupts() ou possíveis modificações.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 
 #define ENDSTOP_PIN 2  //what pin are we using?
 
+unsigned long startmillis;
+unsigned long time = 0;
+float velocidade_rotacao;
+
 void setup() {
   pinMode(ENDSTOP_PIN, INPUT);
   Serial.begin(9600);
+  startmillis = millis();
   Serial.println("Starting opto endstop exerciser.");
   }
 
 void loop() {
+  if (0<abs(time-startmillis)) {
+    time = abs(time-startmillis);
+  }
   if (digitalRead(ENDSTOP_PIN)==true)  {
       Serial.println("open.");
+      velocidade_rotacao = 3.14159 / time * 1000;
+      Serial.println("Velocidade:\t");
+      Serial.println(velocidade_rotacao);
+      Serial.println(" rad/s");
     } else {
       Serial.println("blocked.");
       }
