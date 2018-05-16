4f792016263b5b376243deef8340c2344863326a
Vinícius Alves Pereira <viniciusvap1999@gmail.com>
2018-05-15 23:23:22 -0300

    Adicionado componentes coletores de velocidade. Falta ajustar tempo de calibração

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #define ENDSTOP_PIN 2  //what pin are we using?
 
 unsigned long startmillis;
-unsigned long time = 0;
+unsigned long time_rot;
+unsigned long periodo = 1000;
+unsigned long per_i = 0;
 float velocidade_rotacao;
 
 void setup() {
@@ -14,17 +16,21 @@ void setup() {
   }
 
 void loop() {
-  if (0<abs(time-startmillis)) {
-    time = abs(time-startmillis);
-  }
-  if (digitalRead(ENDSTOP_PIN)==true)  {
+  time_rot = millis();
+  if (periodo<(time_rot-per_i)) {
+    if (digitalRead(ENDSTOP_PIN)==true)  {
+      if (0<abs(time_rot-startmillis)) {
+        startmillis = abs(time_rot-startmillis);
+      }
       Serial.println("open.");
-      velocidade_rotacao = 3.14159 / time * 1000;
+      velocidade_rotacao = (3.14159 / startmillis) * 1000;
       Serial.println("Velocidade:\t");
       Serial.println(velocidade_rotacao);
       Serial.println(" rad/s");
     } else {
-      Serial.println("blocked.");
+        Serial.println(time_rot);
+        Serial.println(startmillis);
       }
-  delay(500);
+    per_i = time_rot - per_i;
+  }
 }
